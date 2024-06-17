b7834b87f4cce61cef122c2792cf0ebc7b1c58cc
Kevin Freire <68775535+Kbakz@users.noreply.github.com>
2024-06-17 10:28:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
-  
+
   $('.carousel').carousel({
     fullWidth: true,
     noWrap: true,
-    indicators: true
+    indicators: true,
+    next: 3
   });
-
+  
+  setInterval(function(){
+    $('.carousel').carousel('next');
+  }, 2000);
+  
   $('select').formSelect();
-
-
 });
